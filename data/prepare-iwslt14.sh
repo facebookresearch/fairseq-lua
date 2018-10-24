cea5fc4031c6ffefe17dc3de10af7b8d5c04f64b
Jonas Gehring <jgehring@fb.com>
2018-10-24 14:47:07 +0200

    Add missing copyright headers

--- data/prepare-iwslt14.sh
+++ data/prepare-iwslt14.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the LICENSE file in
+# the root directory of this source tree. An additional grant of patent rights
+# can be found in the PATENTS file in the same directory.
 #
 # Adapted from https://github.com/facebookresearch/MIXER/blob/master/prepareData.sh
 
